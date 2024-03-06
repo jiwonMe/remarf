237be7f60d4578e2dff702984788659cff817d30
jiwonMe <park@jiwon.me>
2024-03-06 23:52:49 +0900

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/m/:path*',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/plain;charset=UTF-8',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
