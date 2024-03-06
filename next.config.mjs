9080fd2b2dc555f959702026e36ecb857cd33cd1
jiwonMe <park@jiwon.me>
2024-03-06 23:55:40 +0900

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,16 @@ const nextConfig = {
   async headers() {
     return [
       {
+        // 모듈 경로에 대한 MIME 타입 설정
         source: '/m/:path*',
         headers: [
           {
             key: 'Content-Type',
-            value: 'text/plain;charset=UTF-8',
+            value: 'application/javascript',
           },
         ],
       },
+      // 다른 경로에 대한 설정이 필요하다면 여기에 추가
     ];
   },
 };
